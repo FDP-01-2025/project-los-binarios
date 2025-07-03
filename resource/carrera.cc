92d986dc21ac430efffd4507e840081234daa3dc
flaviooozzz <00115625@uca.edu.sv>
2025-07-03 17:57:29 -0600

    Carrera Completa

--- resource/carrera.cc
+++ resource/carrera.cc
@@ -47,10 +47,9 @@ void imprimirPista(int posMiskho, int posRival) {
 }
 
 
-void carrera() {
+int carrera(int rivalVelocidadMs) {
     const int META = 50;
     int posMiskho = 0, posRival = 0;
-    int rivalVelocidadMs = 250;
     DWORD lastRivalMove = GetTickCount();
 
     imprimirPista(posMiskho, posRival);
@@ -77,20 +76,23 @@ void carrera() {
     cout << "\n";
     if (posMiskho >= META && posRival >= META) {
         cout << "EPIC DRAW!\n";
+        return 0;
     } else if (posMiskho >= META) {
         cout << "Miskho wins!\n";
+        return 1;
     } else {
-        cout << "You lost, Rasputin keeps the battery... for now.\n";
+        cout << "You lost\n";
+        return -1;
     }
-
-    getch();
 }
 
+
 int main() {
+    int rivalVelocidadMs = 400, resultado;
     cout << "The conveniently convenient Ol Koi manual starts explaining to Miskho that only 2 parts are missing\n";
     cout << "and the Ol Koi will be his.\n";
-    cout << "The next object is the **battery**, required to make the Ol Koi works.\n";
-    cout << "It is located in the **Electrical Engineering Department**.\n";
+    cout << "The next object is the battery, required to make the Ol Koi work.\n";
+    cout << "It is located in the Electrical Engineering Department.\n";
     cout << "\n[ASCII of energy building here]\n";
     wait(13000);
     system("cls");
@@ -99,12 +101,67 @@ int main() {
     cout << "Rasputin: Hey Miskho! I heard you're looking for this battery.\n";
     cout << "I'm Rasputin, an electrical engineer. I just built this car.\n";
     cout << "If you win a race against me, the battery is yours!\n";
-     wait(11000);
+    wait(11000);
     system("cls");
     cout << "\n[ASCII of Kia Soul here]\n";
     wait(2500);
     system("cls");
 
-    carrera();
+    resultado = carrera(rivalVelocidadMs);
+    wait(1000);
+    system("cls");
+
+    if (resultado == 1) {
+        cout << "Ok, you win this time, but it won't happen again." << endl;
+        cout << "Now, you'll drive this:" << endl;
+        cout << "ASCII bus" << endl;
+        wait(7000);
+        system("cls");
+        cout << "Get ready, because now I will use all I have!" << endl;
+    } else if (resultado == 0) {
+        cout << "Ok, I'll admit that it was a good race. Let's run again to break the draw." << endl;
+        cout << "But now, you'll drive this:" << endl;
+        cout << "ASCII bus" << endl;
+        wait(7000);
+        system("cls");
+        cout << "Get ready, because now I will use all I have!" << endl;
+    } else {
+        cout << "You lost, but I'll give you another chance." << endl;
+        cout << "But now, you'll drive this:" << endl;
+        cout << "ASCII bus" << endl;
+        wait(7000);
+        system("cls");
+        cout << "Get ready, because now I will use all I have!" << endl;
+    }
+
+    rivalVelocidadMs = 100;
+    cout << "NOW IT STARTS!";
+    resultado = carrera(rivalVelocidadMs);
+
+    if (resultado == 1) {
+        cout << "That was quite impressive. I have no doubt you deserve this." << endl;
+        cout << "ASCII battery" << endl;
+        wait(6000);
+        system("cls");
+        cout << "Continue your adventure, you are close to getting what you want." << endl;
+        wait(3500);
+        system("cls");
+    } else if (resultado == 0) {
+        cout << "Well well, that was intense!" << endl;
+        cout << "I think you did a spectacular race, that's why I'll give you what you want." << endl;
+        cout << "ASCII battery" << endl;
+        wait(8000);
+        system("cls");
+        cout << "Continue your adventure, you are close to getting what you want." << endl;
+    } else {
+        cout << "You lost in the clutch time. You don't deserve this." << endl;
+        cout << "Go back and try again if you want (or if you can)." << endl;
+        wait(8000);
+        system("cls");
+        getch();
+        return 0;
+    }
+
+    getch();
     return 0;
 }
